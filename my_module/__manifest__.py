72ab86d00191fcc2957c8856181ffa28953ec985
Silvan Wyden <silvan.wyden@braintec-group.com>
2018-09-25 18:12:20 +0200

    My first module

---
+++ my_module/__manifest__.py
@@ -0,0 +1,17 @@
+##############################################################################
+#
+# Copyright (c) 2017 brain-tec AG (http://www.braintec-group.com)
+# All Right Reserved
+#
+# See LICENSE file for full licensing details.
+##############################################################################
+{
+    'name': "My test module",
+    'summary': "My test module",
+    'author': "Silvan Wyden",
+    'website': "http://www.braintec-group.com/",
+    'category': 'Extra Tools',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL',
+    'depends': ['base'],
+}
